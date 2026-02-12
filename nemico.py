cef7bcdc8490e8232e67da4a2ad9cb8900b52e49
cisuoiamd <zambon.lory@gmail.com>
2026-02-12 11:37:21 +0100

    nemico.py updates

--- nemico.py
+++ nemico.py
@@ -1,9 +1,13 @@
 import arcade
 
 class nemico:
-    def __init__(self, sprite: arcade.Sprite) -> None:
+    def setup(self):
         self.enemy_list = arcade.SpriteList()
     
         enemy= arcade.Sprite("./assets/nemico.png", scale=SPRITE_SCALING)
+        enemy.change_x = 2
+        self.enemy_list.append(enemy)
+        enemy.boundary_right = SPRITE_SIZE * 8
+        enemy.boundary_left = SPRITE_SIZE * 3
         enemy.change_x = 2
         self.enemy_list.append(enemy)
